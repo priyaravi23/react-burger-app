fb198d814069e2ae871c3256b0004c5ebf639c2a
Priya Ravi <priyaravi23@gmail.com>
2021-02-10 08:40:07 -0800

    fixed bugs

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get( 'https://react-burger-app.firebaseio.com/ingredients.json' )
+        axios.get( 'https://make-your-burger-app-default-rtdb.firebaseio.com/ingredients.json' )
             .then( response => {
                dispatch(setIngredients(response.data));
             } )
